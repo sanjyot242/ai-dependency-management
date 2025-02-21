01c0959560c205f279c6c11360e03dd594d63c4a
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2025-02-21 09:11:49 -0800

    Added Dependency Scan Schema

---
+++ node-service/models/DependencyScan.js
@@ -0,0 +1,29 @@
+const mongoosee = require('mongoose');
+
+const DependencySchema = new mongoosee.Schema(
+  {
+    packageName: String,
+    currentVersion: String,
+    latestVersion: String,
+    isOutDated: Boolean,
+  },
+  { _id: false }
+);
+
+const DependencyScanSchema = new mongoosee.Schema({
+  userId: {
+    type: mongoosee.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  repoId: {
+    type: mongoosee.Schema.Types.ObjectId,
+    ref: 'Repository',
+  },
+  scannedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  depedencis: [DependencySchema],
+});
+
+module.exports = mongoosee.model('DependencyScan', DependencyScanSchema);
