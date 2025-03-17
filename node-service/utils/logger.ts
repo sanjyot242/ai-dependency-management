2232c670838d217b1ce69a776b2d0df29f919b9a
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2025-03-16 19:31:15 -0700

    Updated logger from js to ts

--- node-service/utils/logger.ts
+++ node-service/utils/logger.ts
@@ -1,15 +1,14 @@
-// node-service/utils/logger.js
-const { createLogger, format, transports } = require('winston');
+import { createLogger, format, transports, Logger } from 'winston';
 const { combine, timestamp, colorize, printf } = format;
 
 const customFormat = printf(({ level, message, timestamp, ...meta }) => {
-  const metaString = Object.keys(meta).length ? JSON.stringify(meta) : '';
+  const metaString = Object.keys(meta).length
+    ? JSON.stringify(meta, null, 2)
+    : '';
   return `${timestamp} [${level.toUpperCase()}] ${message} ${metaString}`;
 });
-
-const logger = createLogger({
+const logger: Logger = createLogger({
   level: process.env.LOG_LEVEL || 'debug',
-  // No "format" here at the root, because we'll set format per transport.
   transports: [
     // 1) Colorized Console Output
     new transports.Console({
@@ -23,7 +22,6 @@ const logger = createLogger({
     // 2) File Transport (uncolored logs)
     new transports.File({
       filename: 'app.log',
-      // No colorize here
       format: combine(
         timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         customFormat
@@ -32,4 +30,4 @@ const logger = createLogger({
   ],
 });
 
-module.exports = logger;
+export default logger;
